09ed274022e2c4bb016bdd4dce0546193158cb1e
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2019-04-27 02:13:58 +0300

    Поправлены замечания Добавлены новые функции по слайдерам

--- labs/6lab/6_2/js/jquery/preload-image.js
+++
@@ -1,5 +0,0 @@
-new Image().src = "../img/background/slider/start-slide-min.jpg";
-new Image().src = "../img/background/slider/fresh-slide-min.jpg";
-new Image().src = "../img/background/slider/code-slide-min.jpg";
-new Image().src = "../img/background/slider/creative-slide-min.jpg";
-new Image().src = "../img/background/slider/content-slide-min.jpg";
